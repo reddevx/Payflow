a72bb1b77804f97233c9837073ffc60730a8c0a0
reddevx <juan.devx@gmail.com>
2024-06-26 11:14:12 -0300

    feat (update) navbar

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,13 +12,11 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <script src="/static/script.js/script.js"></script>
 </head>
+
 <body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200">
-
- <?php include 'layouts/nav.php'; ?>
- <?php include 'layouts/home.php'; ?>
- <?php include 'layouts/form.php'; ?>
- <?php include 'layouts/graphic.php'; ?>
- <?php include 'layouts/footer.php'; ?>
-
+  <?php include 'layouts/nav.php'; ?>
+  <?php include 'layouts/home.php'; ?>
+  <?php include 'layouts/form.php'; ?>
+  <?php include 'layouts/footer.php'; ?>
 </body>
 </html>
